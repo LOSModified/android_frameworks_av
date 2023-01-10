LOSModified/android_frameworks_av

commit a261106c95bf8d3de4dc06fa3806f8c45bd91a2b

VT: do not remove a rx session even if ECONNREFUSED occurred.
[Problem] rx session removed if recvfrom() returned
 -1 with ECONNREFUSED reason.
[Cause] ECONNREFUSED can be occurred while calling
  recvfrom() after sendto() for a rtcp packet such
  as RR and TMMBR if remote side didn't open a socket yet.
[Solution] rx session should not be removed even if
  remote device didn't ready to receive a packet yet.

Bug: 196252321

Change-Id: I22b1fbd660a930aaac5e0952fb81cf0537822b77
Signed-off-by: Gaurav <gaurav.tiw@samsung.com>

media/libstagefright/rtsp/ARTPConnection.cpp
@@ -508,8 +508,6 @@ void ARTPConnection::onPollStreams() {
                     if (n != (ssize_t)buffer->size()) {
                         ALOGW("failed to send RTCP TMMBR (%s).",
                                 n >= 0 ? "connection gone" : strerror(errno));
-
-                        it = mStreams.erase(it);
                         continue;
                     }
                 }
@@ -560,8 +558,6 @@ void ARTPConnection::onPollStreams() {
                 if (n != (ssize_t)buffer->size()) {
                     ALOGW("failed to send RTCP receiver report (%s).",
                             n >= 0 ? "connection gone" : strerror(errno));
-
-                    it = mStreams.erase(it);
                     continue;
                 }
 
@@ -621,7 +617,14 @@ status_t ARTPConnection::receive(StreamInfo *s, bool receiveRTP) {
     } while (nbytes < 0 && errno == EINTR);
 
     if (nbytes <= 0) {
-        return -ECONNRESET;
+        ALOGW("failed to recv rtp packet. cause=%s", strerror(errno));
+        // ECONNREFUSED may happen in next recvfrom() calling if one of
+        // outgoing packet can not be delivered to remote by using sendto()
+        if (errno == ECONNREFUSED) {
+            return -ECONNREFUSED;
+        } else {
+            return -ECONNRESET;
+        }
     }
 
     buffer->setRange(0, nbytes);
@@ -664,6 +667,10 @@ ssize_t ARTPConnection::send(const StreamInfo *info, const sp<ABuffer> buffer) {
                     info->mRTCPSocket, buffer->data(), buffer->size(), 0,
                     pRemoteRTCPAddr, sizeSockSt);
         } while (n < 0 && errno == EINTR);
+
+        if (n < 0) {
+            ALOGW("failed to send rtcp packet. cause=%s", strerror(errno));
+        }
 
         return n;
 }

commit b56a17bf58e93a6fff8d58680eee9b9bff60eeea

ARTPConnection: Fix the deadloop when network is disconnect while playing rtsp streaming.
The issue was introduced by commit a261106c95bf8d3de4dc06fa3806f8c45bd91a2b
If rever the commit, rtsp will stopped after network disconnect.
rtsp playback can resume with this method when network reconnected.

Bug: 224584833
Test: plugout & plugin network cable while playing rtsp streaming.

Signed-off-by: Ming Zhou <ming.zhou@nxp.com>
Change-Id: I69cf418b064c7019b9f0bac25f3682cbb608eb2d

media/libstagefright/rtsp/ARTPConnection.cpp
@@ -513,6 +513,7 @@ void ARTPConnection::onPollStreams() {
                     if (n != (ssize_t)buffer->size()) {
                         ALOGW("failed to send RTCP TMMBR (%s).",
                                 n >= 0 ? "connection gone" : strerror(errno));
+                        ++it;//prevent deathloop
                         continue;
                     }
                 }
@@ -528,18 +529,16 @@ void ARTPConnection::onPollStreams() {
             || mLastReceiverReportTimeUs + 5000000LL <= nowUs) {
         sp<ABuffer> buffer = new ABuffer(kMaxUDPSize);
         List<StreamInfo>::iterator it = mStreams.begin();
-        while (it != mStreams.end()) {
+        for( ; it != mStreams.end(); ++it) {
             StreamInfo *s = &*it;
 
             if (s->mIsInjected) {
-                ++it;
                 continue;
             }
 
             if (s->mNumRTCPPacketsReceived == 0) {
                 // We have never received any RTCP packets on this stream,
                 // we don't even know where to send a report.
-                ++it;
                 continue;
             }
 
@@ -570,7 +569,6 @@ void ARTPConnection::onPollStreams() {
                 mLastReceiverReportTimeUs = nowUs;
             }
 
-            ++it;
         }
     }
 
@@ -635,7 +633,7 @@ status_t ARTPConnection::receive(StreamInfo *s, bool receiveRTP) {
 
     buffer->setRange(0, nbytes);
 
-    // ALOGI("received %d bytes.", buffer->size());
+    // ALOGI("received %zu bytes.", buffer->size());
 
     status_t err;
     if (receiveRTP) {