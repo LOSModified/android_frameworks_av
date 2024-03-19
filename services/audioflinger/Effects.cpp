f05d6bb77d49518518799d650b35b711ee457777
Shunkai Yao <yaoshunkai@google.com>
2024-03-19 04:27:31 +0000

    audioflinger: Return non-zero channel mask for orphan effects
    Bug: 330202134
    Bug: 309578734
    Test: atest audioeffect_tests
    Test: atest CtsMediaAudioTestCases
    Change-Id: I142409abf56f15e9102df81194fe90c36cfa6be3
    Merged-In: I142409abf56f15e9102df81194fe90c36cfa6be3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/audioflinger/Effects.cpp
+++ services/audioflinger/Effects.cpp
@@ -3141,11 +3141,11 @@ NO_THREAD_SAFETY_ANALYSIS
 {
     const sp<IAfThreadBase> t = thread().promote();
     if (t == nullptr) {
-        return AUDIO_CHANNEL_NONE;
+        return AUDIO_CHANNEL_OUT_STEREO;
     }
     sp<IAfEffectChain> c = chain().promote();
     if (c == nullptr) {
-        return AUDIO_CHANNEL_NONE;
+        return AUDIO_CHANNEL_OUT_STEREO;
     }
 
     if (mThreadType == IAfThreadBase::SPATIALIZER) {
@@ -3180,11 +3180,11 @@ NO_THREAD_SAFETY_ANALYSIS
 {
     const sp<IAfThreadBase> t = thread().promote();
     if (t == nullptr) {
-        return AUDIO_CHANNEL_NONE;
+        return AUDIO_CHANNEL_OUT_STEREO;
     }
     sp<IAfEffectChain> c = chain().promote();
     if (c == nullptr) {
-        return AUDIO_CHANNEL_NONE;
+        return AUDIO_CHANNEL_OUT_STEREO;
     }
 
     if (mThreadType == IAfThreadBase::SPATIALIZER) {
